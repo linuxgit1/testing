5e2417933b3c8bc6c007bfde7f7430c28e9f9070
linuxgit1 <60524663+linuxgit1@users.noreply.github.com>
2021-12-12 10:13:38 +0100

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("custom hello.c from github")                                                                          
+
+    return 0;
+}
