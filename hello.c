39898cfd5b3583911752aabde65d9a79800e4318
linuxgit1 <60524663+linuxgit1@users.noreply.github.com>
2021-12-12 10:14:39 +0100

    Delete hello.c

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-    printf("custom hello.c from github\n")                                                                          
-
-    return 0;
-}
